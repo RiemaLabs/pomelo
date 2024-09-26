629193ed2255ca66c5c792d93f15c694d9fdc7f2
Yanju Chen <chyanju@gmail.com>
2024-09-26 13:33:57 -0700

    added bs standard bv size (520 bytes)

--- pomelo/config.rkt
+++ pomelo/config.rkt
@@ -6,4 +6,5 @@
 ;   - prefix is not recommended when using this as a package
 
 (define ::bvsize 256) ; default bvsize
+; (define ::bvsize 4160) ; default bvsize (520 bytes, bs single element limit)
 (define ::bitvector (bitvector ::bvsize)) ; default bitvector type
