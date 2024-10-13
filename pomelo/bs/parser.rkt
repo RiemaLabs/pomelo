295bd050e071c0d2aea40c61702b44e60199db89
hanzhi <hanzhi@riema.xyz>
2024-10-13 01:12:21 -0700

    OP_NOTIF

--- pomelo/bs/parser.rkt
+++ pomelo/bs/parser.rkt
@@ -62,9 +62,8 @@
        (loop)))))
 
 ; parse tokens into a branching op
-(define/contract (parse-token/branch get)
-  (-> procedure? bs::op::branch?)
-  (define not? (equal? (get) "OP_NOTIF"))
+(define/contract (parse-token/branch get not?)
+  (-> procedure? boolean? bs::op::branch?)
   ; accumulators for then and else branches
   (define thn '())
   (define els '())
@@ -118,10 +117,10 @@
        ; Control Flow (10), opcodes 0x61-0x6a
        [(equal? "OP_NOP" t) (bs::op::nop )]
        [(equal? "OP_VER" t) (bs::op::ver )]
-       [(equal? "OP_IF" t) (parse-token/branch g )]
-       [(equal? "OP_NOTIF" t) (parse-token/branch g )]
-       [(equal? "OP_VERIF" t) (bs::op::verif )]
-       [(equal? "OP_VERNOTIF" t) (bs::op::vernotif )]
+       [(equal? "OP_IF" t) (parse-token/branch g #f)]
+       [(equal? "OP_NOTIF" t) (parse-token/branch g #t)]
+       [(equal? "OP_VERIF" t) (bs::op::verif)]
+       [(equal? "OP_VERNOTIF" t) (bs::op::vernotif)]
        [(equal? "OP_ELSE" t) (bs::op::else )]
        [(equal? "OP_ENDIF" t) (bs::op::endif )]
        [(equal? "OP_VERIFY" t) (bs::op::verify )]
